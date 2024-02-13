7a223dd49921cf3a67264fe04501d7c5e08a8a21
Abdu-Ahmed <159693431+Abdu-Ahmed@users.noreply.github.com>
2024-02-13 09:24:39 +0200

    Update addProducts.php

--- assignment/addProducts.php
+++ assignment/addProducts.php
@@ -8,8 +8,12 @@ $view = new ProductsView();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- css -->
+    <link rel="stylesheet" href="assets\css\styles.css">
     <!-- classless CSS -->
     <link rel="stylesheet" href="https://classless.de/classless.css">
+      <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
      <!-- js -->
     <script src="assets/js/FormAction.js" defer></script>
     <script src="assets/js/FormValidation.js" defer></script>
@@ -19,6 +23,7 @@ $view = new ProductsView();
 <body>
     <!-- submitting products form -->
     <h1>Add Product:</h1>
+     <hr class="my-4">
     <?php $view->skuExists(); ?>
     <form id="product_form" name="form1" onsubmit="return actionOnSubmit();" method="post">
         <div for="prdctattr" class="prdctattr">
@@ -47,8 +52,16 @@ $view = new ProductsView();
         <label for="bookfield" id="bookfield" style="display: none;">Please provide weight:
             <input id="weight" name="weight" type="number" placeholder="Weight (KG)">
         </label>
-        <button id="submit" class="add" type="submit" name="submit">Save</button>
-        <a href="index.php" >Cancel</a>
+        <button id="submit" class="add btn btn-primary" type="submit" name="submit">Save</button>
+        <a href="index.php" class="btn btn-light" >Cancel</a>
     </form>
+          <!-- Footer -->
+      <div class="footer p-3 text-center mt-4">
+      <hr class="my-4">
+        <p>Scandiweb Test Assignment</p>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
